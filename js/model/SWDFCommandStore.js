f4a873181b2e53c66e913fb0b3d1024e2ca4b460
Lionel Médini <lionel.medini@univ-lyon1.fr>
2013-04-14 13:12:17 +0300

    Update SWDFCommandStore.js

--- js/model/SWDFCommandStore.js
+++ js/model/SWDFCommandStore.js
@@ -1,7 +1,7 @@
 /**   
 *	Copyright <c> Claude Bernard - University Lyon 1 -  2013
 * 	License : This file is part of the DataConf application, which is licensed under a Creative Commons Attribution-NonCommercial 3.0 Unported License. See details at : http://liris.cnrs.fr/lionel.medini/wiki/doku.php?id=dataconf&#licensing 
-*   Author: Lionel MEDINI(supervisor), Florian BACLE, Fiona LEPEUTREC, Benoît DURANT-DE-LA-PASTELLIERE, NGUYEN Hoang Duy Tan
+*   Author: Lionel MEDINI(supervisor), Florian BACLE, Fiona LEPEUTREC, Benoï¿½t DURANT-DE-LA-PASTELLIERE, NGUYEN Hoang Duy Tan
 *   Description: This object contains a json definition of all the commands that will prepare all the queries we want to send on the SemanticWebDogFood sparql endpoint.
 *				 Each one of those commands declare the datatype, the method, the query string it is supposed to use on the endpoint and provide the Callback function used to parse the results.		
 *				 To declare a request, each commands can use the parameters declared for the route they are called in (see Configuration.js). This parameter can be a name or an uri and represents
@@ -333,7 +333,7 @@ var SWDFCommandStore = {
 						ViewAdapter.prependToBackboneView('<p>Location : '+(locationName!=""?locationName:eventLocation)+'</p>');   
 					}
 					if(eventLabel != ""){ 
-						ViewAdapter.prependToBackboneView('<h2>'+eventLabel+'</h2>');
+						$("[data-role = page]").find("h1").html(eventLabel);
 					}
 				});            
 			}
